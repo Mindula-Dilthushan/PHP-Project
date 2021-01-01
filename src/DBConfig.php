004f0879e0e96dccf59e57e88b4dc8d0e245af40
Mindula-Dilthushan <66836116+Mindula-Dilthushan@users.noreply.github.com>
2021-01-01 14:12:59 +0530

    php db config

---
+++ src/DBConfig.php
@@ -0,0 +1,8 @@
+<?php
+
+define('HOST',"127.0.0.1");
+define('USERNAME',"root");
+define('PASSWORD',"password");
+define('DATABASE_NAME',"phpdb")
+
+?>
--- src/main.php
+++ src/main.php
@@ -1,5 +1,3 @@
 <?php
-
     echo "This is a PHP Project"
-
 ?>
