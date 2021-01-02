ff91d6a79f62254b625e7e053aeb2af0146770c6
Mindula-Dilthushan <66836116+Mindula-Dilthushan@users.noreply.github.com>
2021-01-02 13:19:50 +0530

    DBConnecter Class Create

---
+++ src/DBConnector.php
@@ -0,0 +1,35 @@
+<?php
+include_once './DBConfig.php';
+
+class DBConnect{
+    private $conn;
+
+    function __construct(){
+        //connect to db
+        $this->connect();
+    }
+
+    function __destruct(){
+        $this->close();
+    }
+
+    function getConnection(){
+        return $this->conn;
+    }
+
+    function connect(){
+        include_once dirname(__FILE__).'DBConfig.php';
+        $this->conn = mysqli_connect(HOST,USERNAME,PASSWORD)or die(mysqli_errno());
+
+        mysqli_set_charset($this->conn,"utf8");
+
+        mysqli_select_db($this->conn,DATABASE_NAME)or die(mysqli_errno());
+
+        return $this->conn;
+    }
+
+    function close(){
+        mysqli_close($this->conn);
+    }
+}
+?>
