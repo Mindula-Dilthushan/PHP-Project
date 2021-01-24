7d6af4ae23411392538f4f7fef7929321bbbcce9
Mindula Dilthushan <minduladilthushan1@gmail.com>
2021-01-24 14:31:54 +0530

    create table

--- src/DBConnector.php
+++ src/DBConnector.php
@@ -1,7 +1,6 @@
 <?php
 include_once './DBConfig.php';
-
-class DBConnect{
+class DBConnector{
     private $conn;
 
     function __construct(){
--- src/DBHandler.php
+++ src/DBHandler.php
@@ -1,12 +1,27 @@
 <?php
     include_once './DBConnector.php';
 
-    $db = new DBConnect();
+    $db = new DBConnector();
 
     $conn = $db->getConnection();
 
     if($conn) {
-        echo "DataBase Connected";
+        echo "DataBase Connected\n";
+
+       $query = "CREATE TABLE student(
+                            stid char(20) PRIMARY KEY,
+                            fname varchar(40),
+                            lname varchar(40),
+                            age int)";
+
+        $respond = mysqli_query($conn,$query);
+
+        if($respond){
+            echo "Table Created";
+        }else{
+            echo "Table Not Created";
+        }
+
     }else{
         echo "DataBase Not Connected";
     }
